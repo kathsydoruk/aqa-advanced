6279adea3f054c97133938e7890f82fe81c758ac
kathsydoruk <kathsydoruk@gmail.com>
2025-10-29 17:51:06 +0200

    Lesson 9 JS Objects examples

---
+++ jsObjects/l9.task1.js
@@ -0,0 +1,8 @@
+const student = {};
+
+student.firstName = "Лера";
+student.lastName = "Банош";
+student.age = 19;
+student.courses = ["Математика", "Історія", "Програмування"];
+
+console.log(student);
---
+++ jsObjects/l9.task2.js
@@ -0,0 +1,10 @@
+const book = {
+  title: "Соляріс",
+  author: "С. Лем",
+  year: 1960,
+};
+
+const { title, author } = book;
+
+console.log(title);
+console.log(author);
